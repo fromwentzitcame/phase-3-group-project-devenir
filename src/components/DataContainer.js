01f7c6e9b85be741336b2ce96e8ba6ffd8849e1a
Alex Wentz <wentz.alex@gmail.com>
2021-11-15 18:02:56 -0500

    get works, data in table

--- src/components/DataContainer.js
+++ src/components/DataContainer.js
@@ -1,10 +1,45 @@
 import React from 'react'
-import DataCard from './DataCard'
+import '../index.css'
 
-function DataContainer() {
+function DataContainer({customers}) {
+    // const displayCustomers = customers.map(customer => {<DataCard key={customer.id} customer={customer} />})
+
+    function renderTableData() {
+        return customers.map(customer => {
+           const { id, name, email, phone, birthday, created_at, updated_at } = customer
+           return (
+              <tr key={id}>
+                 <td>{id}</td>
+                 <td>{name}</td>
+                 <td>{email}</td>
+                 <td>{phone}</td>
+                 <td>{birthday}</td>
+                 <td>{created_at}</td>
+                 <td>{updated_at}</td>
+              </tr>
+           )
+        })
+     }
+
     return (
         <div>
-            <DataCard />
+        <table id='customers' class='margin' border="1">
+          <thead class='blue'>
+            <tr class='padding center'>
+              <th>Customer ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th>Birthday</th>
+              <th>Created At</th>
+              <th>Updated At</th>
+              <th>Edit Customer</th>
+            </tr>
+          </thead>
+          <tbody id="table-body">
+              {renderTableData()}
+          </tbody>
+        </table>
         </div>
     )
 }
