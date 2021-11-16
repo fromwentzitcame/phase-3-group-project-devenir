bfa21862529d52401b7f1a6c09fc7973aa7cee20
Rohail Rafique <rohailrafique@gmail.com>
2021-11-16 11:19:03 -0500

    added_form

--- src/components/AddCustomerForm.js
+++ src/components/AddCustomerForm.js
@@ -1,9 +1,17 @@
 import React from 'react'
 
-function AddCustomerForm() {
+function AddCustomerForm({formData: {name, email, phone, birthday}, handleSubmit, handleFormChange}) {
+    
     return (
         <div>
-            AddCustomerForm Component
+            <h4>Add a customer</h4>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="name" value={name} placeholder="name" onChange={handleFormChange}/>
+                <input type="text" name="email" value={email} placeholder="email" onChange={handleFormChange}/>
+                <input type="text" name="phone" value={phone} placeholder="phone" onChange={handleFormChange}/>
+                <input  type="date" name="birthday" value={birthday} placeholder="birthday" onChange={handleFormChange}/>
+                <button type="submit">Submit</button>
+            </form>
         </div>
     )
 }
