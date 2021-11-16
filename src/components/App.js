609e6b89774d6b89ecdee3d8671c8b9473f588ab
Alex Wentz <wentz.alex@gmail.com>
2021-11-16 15:52:29 -0500

    working filters w bugs

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,9 @@ import React, { useEffect, useState } from "react";
 import "../App.css";
 
 function App() {
+// set states
   const [customers, setCustomers] = useState([]);
+  const [displayedCustomers, setDisplayedCustomers] = useState([])
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -16,36 +18,81 @@ function App() {
     birthday: "",
   });
 
+// first data grab
   useEffect(() => {
     fetch("http://localhost:9292/customers")
       .then((resp) => resp.json())
-      .then((data) => setCustomers(data));
+      .then((data) => {
+        setCustomers(data)
+        setDisplayedCustomers(data)
+      });
   }, []);
 
+// form functionality
   function handleFormChange(e) {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   }
 
   function handleSubmit(e) {
     e.preventDefault();
-
     fetch("http://localhost:9292/customers", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formData)
     })
-    .then(response => response.json())
-    .then(data => {
-      setCustomers([...customers, data])
-      setFormData({
-        name: "",
-        email: "",
-        phone: "",
-        birthday: "",
+      .then(response => response.json())
+      .then(data => {
+        setCustomers([...customers, data])
+        setFormData({
+          name: "",
+          email: "",
+          phone: "",
+          birthday: "",
+        })
       })
-    })
-    ;
   }
+
+// display newsletter subscribers
+function promoFilter(e) {
+  if (e.target.checked) {
+    console.log("filter accessed")
+    fetch("http://localhost:9292/newsletters/promos")
+    .then((resp) => resp.json())
+    .then((data) => setDisplayedCustomers(data));
+  }
+  else setDisplayedCustomers(customers)
+  }
+
+function eventsFilter(e) {
+  if (e.target.checked) {
+    console.log("filter accessed")
+    fetch("http://localhost:9292/newsletters/events")
+    .then((resp) => resp.json())
+    .then((data) => setDisplayedCustomers(data));
+  }
+  else setDisplayedCustomers(customers)
+  }
+
+function birthdayFilter(e) {
+  if (e.target.checked) {
+    console.log("filter accessed")
+    fetch("http://localhost:9292/newsletters/birthdays")
+    .then((resp) => resp.json())
+    .then((data) => setDisplayedCustomers(data));
+  }
+  else setDisplayedCustomers(customers)
+  }
+
+function textsFilter(e) {
+  if (e.target.checked) {
+    console.log("filter accessed")
+    fetch("http://localhost:9292/newsletters/texts")
+    .then((resp) => resp.json())
+    .then((data) => setDisplayedCustomers(data));
+  }
+  else setDisplayedCustomers(customers)
+  }
+
 
   return (
     <div>
@@ -55,7 +102,13 @@ function App() {
         handleFormChange={handleFormChange}
         handleSubmit={handleSubmit}
       />
-      <DataContainer customers={customers} />
+      <DataContainer
+        displayedCustomers={displayedCustomers}
+        promoFilter={promoFilter}
+        eventsFilter={eventsFilter}
+        birthdayFilter={birthdayFilter}
+        textsFilter={textsFilter}
+      />
     </div>
   );
 }
